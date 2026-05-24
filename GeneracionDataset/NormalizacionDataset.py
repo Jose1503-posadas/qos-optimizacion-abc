855ca039dfb9da4751f29e4c5472820164fd1df4
Jose A. Posadas Gudiño <72623826+Jose1503-posadas@users.noreply.github.com>
2026-05-23 20:38:05 -0600

    cambiar rutas del dataset

--- GeneracionDataset/NormalizacionDataset.py
+++ GeneracionDataset/NormalizacionDataset.py
@@ -1,25 +1,25 @@
 import pandas as pd
 
-df = pd.read_csv("DatasetRed.csv")
+# Leer dataset original
+df = pd.read_csv("../Red_datasets/DatasetRed.csv")
 
 df_norm = df.copy()
 
-columnas = ["AnchoBanda", "Latencia", "jitter", "PaquetesPerdidos"]
+columnas = ["AnchoBanda", "Latencia", "jitter","PaquetesPerdidos"]
 
 for col in columnas:
     min_val = df[col].min()
     max_val = df[col].max()
-
-    # evitar división entre cero (por si todos los valores fueran iguales)
+    # evitar división entre cero
     if max_val - min_val != 0:
-        df_norm[col] = (df[col] - min_val) / (max_val - min_val)
+        df_norm[col] = ((df[col] - min_val) /(max_val - min_val))
     else:
         df_norm[col] = 0
 
-# invertir ancho de banda (porque es maximización)
-df_norm["AnchoBanda"] = 1 - df_norm["AnchoBanda"]
+# invertir ancho de banda (maximización -> minimización)
+df_norm["AnchoBanda"] = 1- df_norm["AnchoBanda"]
 
-# Almacenar en CSV
-df_norm.to_csv("DatasetRed_Normalizado.csv", index=False)
+# Guardar dataset normalizado
+df_norm.to_csv("../Red_datasets/DatasetRed_Normalizado.csv",index=False)
 
 print(df_norm.describe())
